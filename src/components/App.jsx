412b86f3fbc63dc5fae9236aa54efdc7afbe0d23
Roman Kolobych <romankol1223@gmail.com>
2023-05-16 19:30:05 +0300

    localStorege

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,23 @@ import ContactForm from './ContactForm/ContactForm';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
 
     filter: '',
   };
+
+  componentDidMount() {
+    const localData = localStorage.getItem('contacts');
+    if (localData) {
+      this.setState({ contacts: JSON.parse(localData) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleInputChange = event => {
     const { name, value } = event.target;
